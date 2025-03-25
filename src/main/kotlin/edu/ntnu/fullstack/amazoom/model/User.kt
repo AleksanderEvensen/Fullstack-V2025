4a7db38af2d453d6c4fd429b0fd079e3952e138b
Aleksander Evensen <ahse03@gmail.com>
2025-03-25 13:09:13 +0100

    Added initial user model

---
+++ src/main/kotlin/edu/ntnu/fullstack/amazoom/model/User.kt
@@ -0,0 +1,23 @@
+package edu.ntnu.fullstack.amazoom.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "first_name", nullable = false)
+    var firstName: String,
+
+    @Column(name = "last_name", nullable = false)
+    var lastName: String,
+
+    @Column(nullable = false)
+    var email: String
+)
