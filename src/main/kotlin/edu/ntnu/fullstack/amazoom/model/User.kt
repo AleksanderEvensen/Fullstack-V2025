b31b4cf4c8bcdb31a2cf3f5ce0869767ea7a2f4d
yazanzarka1 <yazanzarka1@gmail.com>
2025-03-28 13:53:09 +0100

    feat(backend): sets up database and backend

--- src/main/kotlin/edu/ntnu/fullstack/amazoom/model/User.kt
+++ src/main/kotlin/edu/ntnu/fullstack/amazoom/model/User.kt
@@ -1,23 +1,29 @@
 package edu.ntnu.fullstack.amazoom.model
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
+import java.time.LocalDateTime
 
-@Entity
-class User(
+@Entity(name = "users")
+data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 
-    @Column(name = "first_name", nullable = false)
-    var firstName: String,
+    @Column(nullable = false, unique = true)
+    var name: String,
 
-    @Column(name = "last_name", nullable = false)
-    var lastName: String,
+    @Column(nullable = false, unique = true)
+    var email: String,
 
     @Column(nullable = false)
-    var email: String
+    var password: String,
+
+    @Column(nullable = false)
+    var enabled: Boolean = false,
+
+    @Column(name = "created_at")
+    var createdAt: LocalDateTime? = null,
+
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime? = null
 )
---
+++ src/main/resources/db/migration/V4__add_email_to_user.sql
@@ -0,0 +1,4 @@
+
+
+ALTER TABLE users ADD  COLUMN email VARCHAR(100) NOT NULL,
+ADD CONSTRAINT uc_user_email UNIQUE (email);
