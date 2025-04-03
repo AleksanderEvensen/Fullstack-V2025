bcaf2f05e0fa09d1036e607db46920273b66ca56
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-04-03 19:06:05 +0200

    feat: add security config for requests

---
+++ src/main/kotlin/edu/ntnu/fullstack/amazoom/auth/config/SecurityConfig.kt
@@ -0,0 +1,100 @@
+package edu.ntnu.fullstack.amazoom.auth.config
+
+import JwtAuthFilter
+import edu.ntnu.fullstack.amazoom.auth.filter.RefreshTokenFilter
+import edu.ntnu.fullstack.amazoom.auth.repository.RefreshTokenRepository
+import edu.ntnu.fullstack.amazoom.auth.service.JwtService
+import edu.ntnu.fullstack.amazoom.auth.service.UserDetailsService
+import edu.ntnu.fullstack.amazoom.auth.util.CookieUtils
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig(
+    private val jwtService: JwtService,
+    private val userDetailsService: UserDetailsService,
+    private val cookieUtils: CookieUtils,
+    private val refreshTokenRepository: RefreshTokenRepository,
+    private val authProperties: AuthProperties,
+) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors { cors ->
+                cors.configurationSource(corsConfigurationSource())
+            }
+//            .csrf { csrf ->
+//                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                csrf.csrfTokenRequestHandler(CsrfTokenRequestAttributeHandler().apply {
+//                    setCsrfRequestAttributeName(null)
+//                })
+//            }
+            .csrf {
+                it.disable()
+            }
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers("/auth/register", "/auth/login").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(
+                JwtAuthFilter(jwtService, userDetailsService, cookieUtils),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
+            .addFilterBefore(
+                RefreshTokenFilter(
+                    jwtService,
+                    refreshTokenRepository,
+                    userDetailsService,
+                    cookieUtils,
+                    authProperties
+                ), JwtAuthFilter::class.java
+            )
+
+        return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().apply {
+            allowedOrigins = listOf("http://localhost:3000")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+            exposedHeaders = listOf("X-XSRF-TOKEN")
+            maxAge = 3600L
+        }
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun authenticationManager(authConfig: AuthenticationConfiguration): AuthenticationManager {
+        return authConfig.authenticationManager
+    }
+}
