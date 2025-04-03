41ee0b171b45a9e90de534dc6fdd80ba5b99a887
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-04-03 19:07:10 +0200

    feat: data class for jwt application properties

---
+++ src/main/kotlin/edu/ntnu/fullstack/amazoom/auth/config/AuthProperties.kt
@@ -0,0 +1,12 @@
+package edu.ntnu.fullstack.amazoom.auth.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "app.auth")
+data class AuthProperties (
+    val jwtSecret: String,
+    val jwtIssuer: String,
+    val accessTokenExpiration: Long,
+    val refreshTokenExpiration: Long,
+    val secureCookie: Boolean
+)
